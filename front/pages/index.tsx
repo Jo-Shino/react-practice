3a544d5ca17ce3b3568691d8ed2df9c8803b4e91
Jo-Shino <joe000042@gmail.com>
2023-08-15 18:24:11 +0900

    formにstringとnumberを増やして対応

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,17 @@
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { TextField } from "@mui/material/";
 
-interface IFormInputs {
-  TextField: number;
-  MyCheckbox: boolean;
+interface FormInput {
+  peopleName: string;
+  title: string;
+  price: number;
+  quantity: number;
+  content: string;
+  privilege: string;
+  constrant: string;
+  confirmation: string;
+  videoUrl: string;
+  docURL: string;
 }
 
 export default function App() {
@@ -11,13 +19,136 @@ export default function App() {
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm<IFormInputs>();
-  const onSubmit: SubmitHandler<IFormInputs> = (data: IFormInputs) =>
+    watch,
+  } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data: FormInput) =>
     console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {/* peopleName */}
+      {/* notice: valueの箇所で問題あり */}
       <Controller
+        name="peopleName"
+        control={control}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (value !== null && value !== "") {
+              return true;
+            }
+            return "文字を入力してください";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="peopleName"
+            size="small"
+            sx={{ width: 500 }}
+            disabled
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.peopleName?.message : ""}
+          />
+        )}
+      />
+      {/* title */}
+      <Controller
+        name="title"
+        control={control}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (value !== null || "") {
+              return true;
+            }
+            return "文字を入力してください";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="title"
+            size="small"
+            sx={{ width: 500 }}
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.title?.message : ""}
+          />
+        )}
+      />
+      {/* price */}
+      <Controller
+        name="price"
+        control={control}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (!Number.isNaN(Number(value))) {
+              return true;
+            }
+            return "数値を入力してください(全角不可)";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="price"
+            size="small"
+            sx={{ width: 500 }}
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.price?.message : ""}
+          />
+        )}
+      />
+      {/* quantity */}
+      <Controller
+        name="quantity"
+        control={control}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (!Number.isNaN(Number(value))) {
+              return true;
+            }
+            return "数値を入力してください(全角不可)";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="quantity"
+            size="small"
+            sx={{ width: 500 }}
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.quantity?.message : ""}
+          />
+        )}
+      />
+      {/* content */}
+      <Controller
+        name="content"
+        control={control}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (value !== null || "") {
+              return true;
+            }
+            return "文字を入力してください";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="content"
+            size="small"
+            sx={{ width: 500 }}
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.content?.message : ""}
+          />
+        )}
+      />
+      {/* <Controller
         name="TextField"
         control={control}
         rules={{
@@ -35,12 +166,12 @@ export default function App() {
             type="TextField"
             label="TextField"
             fullWidth
-            placeholder="012345678"
+            placeholder="0123456789"
             error={fieldState.invalid}
             helperText={fieldState.invalid ? errors.TextField?.message : ""}
           />
         )}
-      />
+      /> */}
       <input type="submit" />
     </form>
   );
