2be88854b71522e017974c7fda808d7ee299b8af
Jo-Shino <joe000042@gmail.com>
2023-08-15 16:30:54 +0900

    数値じゃないと入力を受け付けないようにvalidateした

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,26 +1,45 @@
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import { TextField, Checkbox } from "@mui/material/";
+import { TextField } from "@mui/material/";
 
 interface IFormInputs {
-  TextField: string;
+  TextField: number;
   MyCheckbox: boolean;
 }
 
 export default function App() {
-  const { handleSubmit, control, reset } = useForm<IFormInputs>({
-    defaultValues: {
-      MyCheckbox: false,
-    },
-  });
-  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFormInputs>();
+  const onSubmit: SubmitHandler<IFormInputs> = (data: IFormInputs) =>
+    console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Controller
-        name="MyCheckbox"
+        name="TextField"
         control={control}
-        rules={{ required: true }}
-        render={({ field }) => <Checkbox {...field} />}
+        rules={{
+          required: { value: true, message: "必須入力" },
+          validate: (value) => {
+            if (!Number.isNaN(Number(value))) {
+              return true;
+            }
+            return "数値を入力してください(全角不可)";
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            type="TextField"
+            label="TextField"
+            fullWidth
+            placeholder="012345678"
+            error={fieldState.invalid}
+            helperText={fieldState.invalid ? errors.TextField?.message : ""}
+          />
+        )}
       />
       <input type="submit" />
     </form>
